d69bc952221884edc73092e5a4aaf9713a12177f
Git-Permormer <jhonatapereirag@outlook.com>
2024-12-30 00:04:34 -0300

    Atualiza arquivo ci.yml e requirements.txt

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '6cyytc',
-    e2e: {
-        baseUrl: "http://127.0.0.1:5000", // URL base do servidor backend
-    },
-});
+  e2e: {
+    baseUrl: "http://127.0.0.1:5000", // URL base do servidor backend
 
+    setupNodeEvents(on, config) {
+      // Registra a tarefa 'log' que vai exibir as mensagens no console
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+    },
+  },
+});
